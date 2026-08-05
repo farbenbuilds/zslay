4796ef71e255d34ef04ba161838979e1674cf26a
Noah Trần <trananhquan1009@gmail.com>
2026-08-06 02:26:31 +0700

    refactor: streamline comments in build.zig

--- build.zig
+++ build.zig
@@ -4,16 +4,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // export pure Zig module for other Zig projects integration
-    // registered with the name "zslay"
+    // export pure Zig module
     _ = b.addModule("zslay", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // build C-compatible static library through src/c_api.zig
-    // compiles exported symbols with C calling conventions
+    // build C-compatible static library
     const lib = b.addStaticLibrary(.{
         .name = "zslay",
         .root_module = b.createModule(.{
@@ -23,11 +21,9 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    // output compiled library
     b.installArtifact(lib);
 
-    // setup local unit tests from src/test.zig
-    // executed natively via "zig build test" inside Nix
+    // setup local unit tests
     const lib_tests = b.addTest(.{
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/test.zig"),
@@ -38,7 +34,6 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_tests = b.addRunArtifact(lib_tests);
 
-    // expose the build to the CLI
     const test_step = b.step("test", "Run unit tests for zslay");
     test_step.dependOn(&run_lib_tests.step);
 }
