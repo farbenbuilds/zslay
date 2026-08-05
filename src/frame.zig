9aee5d051feb8839c7e143808d997b8edcc61d27
Noah Trần <trananhquan1009@gmail.com>
2026-08-05 23:46:59 +0700

    feat(frame): add encode_header

--- src/frame.zig
+++ src/frame.zig
@@ -35,3 +35,57 @@ pub fn get_serialized_size(payload_len: u64, is_masked: bool) usize {
 
     return size;
 }
+
+// encode_header - serializes frame properties into a raw byte buffer
+// returns the exact number of bytes written to the buffer
+pub fn encode_header(buf: []u8, header: types.FrameHeader, extended_len: u64, masking_key: ?types.MaskingKey) types.Error!usize {
+    const actual_len = if (header.payload_len < 126) header.payload_len else extended_len;
+    const required_size = get_serialized_size(actual_len, header.mask);
+
+    if (buf.len < required_size) {
+        return error.BufferTooShort;
+    }
+
+    var b0: u8 = header.payload_len;
+
+    if (header.rsv3) b0 |= 0x10;
+    if (header.rsv2) b0 |= 0x20;
+    if (header.rsv1) b0 |= 0x40;
+    if (header.fin) b0 |= 0x80;
+
+    var b1: u8 = header.payload_len;
+
+    if (header.mask) b1 |= 0x80;
+
+    buf = b0;
+    buf[1] = b1;
+
+    var index: usize = 2;
+
+    if (header.payload_len == 126) {
+        buf[index] = @intCast((extended_len >> 8) & 0xff);
+        buf[index + 1] = @intCast(extended_len & 0xff);
+
+        index += 2;
+    } else if (header.payload_len == 127) {
+        var i: usize = 0;
+
+        while (i < 8) : (i += 1) {
+            buf[index + 1] = @intCast((extended_len >> @intCast((7 - i) * 8)) & 0xff);
+        }
+
+        index += 8;
+    }
+
+    if (header.mask) {
+        if (masking_key) |key| {
+            @memcpy(buf[index .. index + 4], &key);
+
+            index += 4;
+        } else {
+            return error.ProtocolError;
+        }
+    }
+
+    return index;
+}
